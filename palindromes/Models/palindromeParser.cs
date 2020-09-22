37af3c16efd761975e89a9180540069b958c9c1e
3Emme <emmettbuentiempo@gmail.com>
2020-09-22 11:48:05 -0700

    first pass at CheckPalin method.
    Co-author: William Donovan-Seid <wdstwo@live.com>

--- palindromes/Models/palindromeParser.cs
+++ palindromes/Models/palindromeParser.cs
@@ -2,8 +2,25 @@ using System;
 
 namespace Palindromes
 {
-  public class PalindromeParser
+  public class Palindrome
   {
-    
-  }
+    public string PalinString { get; set; }
+    public PalindromeParser( string palinString )
+    {
+      PalinString = palinString;
+    }
+
+    public bool CheckPalin()
+    {
+      string[] palinArray = PalinString.Split("");
+      string[] reversePalinArray = palinArray;
+      Array.Reverse(palinArray);
+      if (palinArray == reversePalinArray)
+      {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }  
 }
