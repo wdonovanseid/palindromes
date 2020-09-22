9038938e0741207a0e744f9a1b287e1bb9889eea
3Emme <emmettbuentiempo@gmail.com>
2020-09-22 14:05:18 -0700

    write frontend functions.
    Co-author: William Donovan-Seid <wdstwo@live.com>

--- palindromes/Models/program.cs
+++ palindromes/Models/program.cs
@@ -1,9 +1,31 @@
-// using System;
+using System;
 
-// namespace Palindromes
-// {
-//   class Program
-//   {
-//     //etc
-//   }
-// }
+namespace Palindromes
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Enter word: ");
+      string userWord = Console.ReadLine();
+
+      Palindrome palin = new Palindrome(userWord);
+
+      if (palin.CheckPalin())
+      {
+        Console.WriteLine("------------------------");
+        Console.WriteLine("-                      -");
+        Console.WriteLine("-  It's a Palindrome!  -");
+        Console.WriteLine("-                      -");
+        Console.WriteLine("------------------------");
+      } else 
+      {
+        Console.WriteLine("------------------------");
+        Console.WriteLine("-                      -");
+        Console.WriteLine("-  Not a Palindrome!!  -");
+        Console.WriteLine("-                      -");
+        Console.WriteLine("------------------------");
+      }
+    }
+  }
+}
