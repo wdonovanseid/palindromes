16e21d47cde7c6a86c3ccd179b46d96a5129fc8f
3Emme <emmettbuentiempo@gmail.com>
2020-09-22 11:25:24 -0700

    continue to create initial program strucute.
    Co-author: William Donovan-Seid <wdstwo@live.com>

---
+++ palindromes.Tests/ModelTests/palindromeParser.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PalindromeParser;
+
+namespace PalindromeParser.Tests
+{
+  [TestClass]
+  public class PalindromParserTets
+  {
+    //etc
+  }
+}
---
+++ palindromes/Models/program.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Palindromes
+{
+  public class Main
+  {
+    //etc
+  }
+}
